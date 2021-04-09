11cc94248fc94cc3a757d0f7a0b7aeec1bd34440
nombre <daniel.b100726@gmail.com>
2021-04-09 14:06:48 -0400

    capturando los botones

--- main.js
+++ main.js
@@ -1,3 +1,127 @@
-window.onload = () => {
-    display = document.getElementById("display-calculator")
+// window.onload = () => {
+//     display = document.getElementById("display-calculator")
+// }
+
+const captureBtn1 = () => {
+    const btn = document.getElementById("btn-1").value
+    console.log(btn)
+    return btn
 }
+
+const captureBtn2 = () => {
+    const btn = document.getElementById("btn-2").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn3 = () => {
+    const btn = document.getElementById("btn-3").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn4 = () => {
+    const btn = document.getElementById("btn-4").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn5 = () => {
+    const btn = document.getElementById("btn-5").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn6 = () => {
+    const btn = document.getElementById("btn-6").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn7 = () => {
+    const btn = document.getElementById("btn-7").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn8 = () => {
+    const btn = document.getElementById("btn-8").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn9 = () => {
+    const btn = document.getElementById("btn-9").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtn0 = () => {
+    const btn = document.getElementById("btn-0").value
+    console.log(btn)
+    return btn
+}
+const captureBtnSum = () => {
+    const btn = document.getElementById("btn-+").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnRest = () => {
+    const btn = document.getElementById("btn--").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnDiv = () => {
+    const btn = document.getElementById("btn-/").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnMult = () => {
+    const btn = document.getElementById("btn-*").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnDecimal = () => {
+    const btn = document.getElementById("btn-.").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnSig = () => {
+    const btn = document.getElementById("btn-+/-").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnEqual = () => {
+    const btn = document.getElementById("btn-=").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnDel = () => {
+    const btn = document.getElementById("btn-del").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnCe = () => {
+    const btn = document.getElementById("btn-ce").value
+    console.log(btn)
+    return btn
+}
+
+const captureBtnC = () => {
+    const btn = document.getElementById("btn-c").value
+    console.log(btn)
+    return btn
+}
+
+// captureBtn("btn-del")
+
+// display = document.getElementById("display-calculator")
+// console.log(display)
