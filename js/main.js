10455e46d7cd50d582421f4c84ced2e8559b1e71
nombre <daniel.b100726@gmail.com>
2021-04-10 23:45:53 -0400

    añadida parcialmente funcionalidad para usar calculadora con el teclado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     display = document.getElementById("display-calculator")
-    // document.onkeydown = keyboard
+    document.onkeydown = keyboard
 }
 
 let saveDisplay = "0" //Display inicial = 0
@@ -116,8 +116,8 @@ const changeSig = () => {
 
 const btnDel = () => {//borramos solo el último número escrito
     let figures = saveDisplay.length //figures => cifras (de números)
-    let del = saveDisplay.substr(figures - 1,figures)//información sobre el último caracter escrito
-    saveDisplay = saveDisplay.substr(0, figures - 1)//eliminar el último caracter
+    let del = saveDisplay.substring(figures - 1,figures)//información sobre el último caracter escrito
+    saveDisplay = saveDisplay.substring(0, figures - 1)//eliminar el último caracter
     if (saveDisplay == "") {//si no quedan caracteres, hacer cero el display
         saveDisplay = "0"
     }
@@ -168,4 +168,43 @@ const errorAction = () => {
         auxSolve = 0
         solve = 0
     }, 1500)
+}
+
+const keyboard = (ev) => {
+    const eve = ev || window.event
+    let key = eve.keyCode //número de la tecla
+    if ((key > 47) && (key < 58)) { //teclas númericas del teclado alfamunérico
+        let code = key - 48 //buscar número a mostrar
+        code = code.toString()
+        captureBtn(code) //llamada a la tecla numérica correspondiente
+    }
+    if ((key > 95) && (key < 106)) { //Teclas del teclado númerico.
+        let code = key - 96 //buscar número a mostrar
+        code = code.toString()
+        captureBtn(code) //llamada a la tecla numérica correspondiente
+    }
+    if ((key == 110) || (key == 190)) {
+        captureBtn(".")
+    }
+    if (key == 106) {
+        operation("*")
+    }
+    if (key==109) {
+        operation('-')
+    } 
+    if (key==111) {
+        operation('/')
+    }
+    if (key==32 || key==13) {
+        equal()
+    }
+    if (key==46) {
+        btnC()
+    }
+    if (key==36) {
+        btnCe()
+    }
+    if (key==8) {
+        btnDel()
+    }
 }
